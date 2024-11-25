b71a585cd661d61fa60c23c6c5dc3155fad06142
JamesTylerBurgess <91066330+jtb19833@users.noreply.github.com>
2024-11-25 11:34:20 -0500

    Minor search update

--- weathertabs/src/app/components/TabAdder.js
+++ weathertabs/src/app/components/TabAdder.js
@@ -74,7 +74,7 @@ export default function TabAdder () {
     return (
         <div className="flex flex-col items-center self-center p-10 m-10 w-full max-w-[1200px] bg-indigo-300 rounded-3xl gap-5">
             <h2 className="text-3xl font-bold text-white">Add New Tab</h2>
-            <div className="flex flex-col bg-inherit items-center justify-space-between self-center w-fill" onSubmit={handlesubmit}>
+            <div className="flex flex-col bg-inherit items-center justify-space-between self-center w-fill" >
                 <input type="text" className= "font-medium text-lg bg-white text-black min-w-[500px] py-2 px-1 rounded-lg" onChange={(e) => setLocation(e.target.value)} value={location} placeholder={"Search Locations..."}></input>
                 {AzureLocations.length >0 ?
                 <div className="flex flex-col bg-white mt-11 w-[500px] absolute self-center justify-start items-start text-lg decoration-1 rounded-xl">
@@ -104,7 +104,7 @@ export default function TabAdder () {
                     </div>
                 </div>
                 <div className="flex flex-row bg-inherit text-white justify-space-between items-center self-center min-w-500 min-h-fit gap-10">
-                    <button type="submit" className="font-bold text-lg bg-blue-600 text-white py-2 px-4 rounded-lg">Add Tab</button>
+                    <button type="submit" className="font-bold text-lg bg-blue-600 text-white py-2 px-4 rounded-lg" onClick={handlesubmit}>Add Tab</button>
                     <button className="font-bold text-lg bg-gray-400 text-white py-2 px-4 rounded-lg me-5" onClick={handleCancel}>Cancel</button>
                 </div>
             </div>
